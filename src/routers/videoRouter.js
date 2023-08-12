92a0842065c8c0cce4b12b45a435c187867a3f30
ponyoung <icepower1234@naver.com>
2023-08-12 13:08:03 +0900

    Set basic routers
    With studying URL parameters and some RE.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+// 업로드를 위에 두지 않으면 "upload"를 id로 인식함 -> 숫자만 인식하기 때문에 밑에 가도 상관 없음.
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
