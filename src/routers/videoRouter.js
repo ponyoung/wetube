f851e7ba77c1f8189631830ba923f474959af47c
ponyoung <icepower1234@naver.com>
2023-08-17 18:39:38 +0900

    (practiced post request)
    With uploading new video.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 import {
   watch,
   getEdit,
-  upload,
-  deleteVideo,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,5 +12,6 @@ const videoRouter = express.Router();
 // 업로드를 위에 두지 않으면 "upload"를 id로 인식함 -> 숫자만 인식하기 때문에 밑에 가도 상관 없음.
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
