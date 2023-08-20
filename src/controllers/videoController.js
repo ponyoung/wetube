2e19b06f6ab7c751f79b2f38e94261eab825bc65
ponyoung <icepower1234@naver.com>
2023-08-20 13:15:35 +0900

    Add video with real database
    cool

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,10 +6,11 @@ export const home = async (req, res) => {
   const videos = await Video.find({}); ///// js will wait
   return res.render("home", { pageTitle: "Home", videos }); // return보다 호출하는 함수가 중요함 (render을 두번 호출할 수 없음.)
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   // const id = req.params.id ---> ES6!!
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -24,7 +25,20 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    // 스키마를 토대로 object가 생성됨
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,8 +10,8 @@ import {
 const videoRouter = express.Router();
 
 // 업로드를 위에 두지 않으면 "upload"를 id로 인식함 -> 숫자만 인식하기 때문에 밑에 가도 상관 없음.
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
