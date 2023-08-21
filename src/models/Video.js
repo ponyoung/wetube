c4548525f35acdaf6d321cdc9f78fafd26e997f7
ponyoung <icepower1234@naver.com>
2023-08-21 23:25:12 +0900

    Edit video with real database
    Format hashtags with static! (Video.~~)
    Also studied mongoose middlewares.

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,12 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+// Video.formatHashtags로 사용가능 !!
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
