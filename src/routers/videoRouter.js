f060e49b76a28633c7f5e9b26dc5df1c9709cff1
ponyoung <icepower1234@naver.com>
2023-08-22 16:57:37 +0900

    Search videos
    With re!
    CRUD complete with MongoDB, Mongoose!

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,6 +13,6 @@ const videoRouter = express.Router();
 // 업로드를 위에 두지 않으면 "upload"를 id로 인식함 -> 숫자만 인식하기 때문에 밑에 가도 상관 없음.
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-
 export default videoRouter;
