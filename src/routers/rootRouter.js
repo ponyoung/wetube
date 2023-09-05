6d68295e5fccf77be858301156b4a3314858d71e
ponyoung <icepower1234@naver.com>
2023-09-05 19:00:35 +0900

    Protect middlewares and Edit profile
    Protect some urls with middlewares.

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,12 +6,17 @@ import {
   postLogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { pubilcOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(pubilcOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(pubilcOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,20 @@
 import express from "express";
 import {
-  edit,
   logout,
   see,
   startGithubLogin,
   finishGithubLogin,
+  getEdit,
+  postEdit,
 } from "../controllers/userController";
+import { protectorMiddleware, pubilcOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", pubilcOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", pubilcOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
