9de6f3d74ae777fee6983e4ad41ff3a77ae67e99
ponyoung <icepower1234@naver.com>
2023-09-06 20:05:40 +0900

    Edit profile informations

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,12 +7,24 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // 업로드를 위에 두지 않으면 "upload"를 id로 인식함 -> 숫자만 인식하기 때문에 밑에 가도 상관 없음.
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 export default videoRouter;
